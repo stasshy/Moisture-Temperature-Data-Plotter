a8116df15792b1d7173a86534d162f47a8e2de0c
AnastA <155085436+stasshy@users.noreply.github.com>
2025-05-18 15:55:44 +0300

    Update startGraph.py

--- startGraph.py
+++ startGraph.py
@@ -1,103 +1,41 @@
 import time
-
 import serial
-
 import matplotlib.pyplot as plt
-
 import matplotlib.animation as animation
 
-
-
-
-
-def animate(i, moistureList,tempList ,ser):
-
-    ser.write(b'g')                                     
-
-    Data_string = ser.readline().decode('ascii') 
-
-    #print(i)                                           
-
-
-
+def animate(i, moistureList, tempList, ser):
+    ser.write(b'g')
+    data_string = ser.readline().decode('ascii')
     try:
-
-        tmpList = Data_string.split(",")
-
+        tmpList = data_string.split(",")
         moistureData_float = float(tmpList[0])
-
-        tempData_float = float(tmpList[1])   
-
-        moistureList.append(moistureData_float) 
-
+        tempData_float = float(tmpList[1])
+        moistureList.append(moistureData_float)
         tempList.append(tempData_float)
-
-        print(tempData_float)            # Add to the list holding the fixed number of points to animate
-
-
-
-    except:                                                                       
-
+    except:
         pass
-
-
-
-    moistureList = moistureList[-50:]
-
-    tempList = tempList[-50:]                           
-
-    
-
-    ax.clear()                                         
-
+    moistureList[:] = moistureList[-50:]
+    tempList[:] = tempList[-50:]
+    ax.clear()
     ax.plot(moistureList)
-
     ax2.clear()
-
-    ax2.plot(tempList,'r')                                   
-
-    
-
+    ax2.plot(tempList, 'r')
     ax.set_ylim([0, 100])
+    ax2.set_ylim([1, 45])
+    ax.set_title("Moisture and Temperature data")
+    ax.set_ylabel("Moisture", color="blue", fontsize=14)
+    ax2.set_ylabel("Temperature", color="red", fontsize=14)
 
-    ax2.set_ylim([1,45])                              
-
-    ax.set_title("Moisture and Temperature data")                        # Set title of figure
-
-    ax.set_ylabel("Moisture",color="blue",fontsize=14)
-
-    ax2.set_ylabel("Temperature",color="red",fontsize=14)                              # Set title of y axis 
-
-
-
-moistureList = [] 
-
+moistureList = []
 tempList = []
 
-                                                        
-
-fig = plt.figure()                                      
-
+fig = plt.figure()
 ax = fig.add_subplot(111)
+ax2 = ax.twinx()
 
-ax2 = ax.twinx()                               
+ser = serial.Serial("COM6", 9600)  # Change as needed
+time.sleep(2)
 
-
-
-ser = serial.Serial("COM6", 9600)                       # Establish Serial object with COM port and BAUD rate to match Arduino Port/rate
-
-time.sleep(2)                                           
-
-
-
-                                                        # Matplotlib Animation Fuction that takes takes care of real time plot.
-
-                                                        
-
-ani = animation.FuncAnimation(fig, animate, frames=30, fargs=(moistureList, tempList, ser), interval=100) 
-
-
-ani.save("test1.gif")
-plt.show()                                              # Keep Matplotlib plot persistent on screen until it is closed
-
-ser.close() 
+ani = animation.FuncAnimation(fig, animate, frames=30, fargs=(moistureList, tempList, ser), interval=100)
+plt.show()
+ser.close()
